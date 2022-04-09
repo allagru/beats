fca99dd964c4c00c1bdc50d364cf71a8a0ef94e6
allagru <58115081+allagru@users.noreply.github.com>
2022-04-09 16:59:24 +0300

    исправления 5 недели

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const { src, dest, task, series, watch } = require("gulp");
+const rm = require( "gulp-rm" );
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
+const px2rem = require('gulp-smile-px2rem');
+const gcmq = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+task( "clean", () => {
+    return src( "dist/*", { read: false }).pipe( rm() );
+});
+
+task ("copy:html", () => {
+    return src('*.html')
+        .pipe(dest('dist'))
+        .pipe(reload({ stream: true }));
+    });
+
+const styles = [
+    "node_modules/normalize.css/normalize.css",
+    "beats/scss/main.scss"
+];
+
+task ("styles", () => {
+    return src(styles)
+      .pipe(sourcemaps.init())
+      .pipe(concat('main.scss'))
+      .pipe(sassGlob())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(px2rem())
+      .pipe(autoprefixer({
+        cascade: false
+    }))
+    //   .pipe(gcmq())
+      .pipe(cleanCSS())
+      .pipe(uglify())
+      .pipe(sourcemaps.write())
+      .pipe(dest('dist'));
+});
+
+task('scripts', () => {
+    return src("beats/js/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js', {newLine: ";"}))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(dest('dist'));
+})
+
+task('server', () => {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        },
+        open: false 
+    });
+});
+
+watch('beats/scss/**/*.scss', series('styles'));
+watch("*.html", series("copy:html"));
+watch("beats/js/*.js", series("scripts"));
+
+task("default", series("clean", "copy:html", "styles", "scripts","server"));
+
+
